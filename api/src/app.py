9e175d3e893ae871180b23c11ec978d6d576ef6e
Taesung Hwang <taesungh@uci.edu>
2023-10-19 18:00:34 -0700

    Create `Quote` type and rename posts to quotes

--- api/src/app.py
+++ api/src/app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Any
+from typing import TypedDict
 
 from fastapi import FastAPI, Form, status
 from fastapi.responses import RedirectResponse
@@ -8,15 +8,22 @@ from services.database import JSONDatabase
 
 app = FastAPI()
 
-database: JSONDatabase[list[dict[str, Any]]] = JSONDatabase("data/database.json")
+
+class Quote(TypedDict):
+    name: str
+    message: str
+    time: str
+
+
+database: JSONDatabase[list[Quote]] = JSONDatabase("data/database.json")
 
 
 @app.on_event("startup")
 def on_startup() -> None:
     """Initialize database when starting API server."""
-    if "posts" not in database:
-        print("Adding posts entry to database")
-        database["posts"] = []
+    if "quotes" not in database:
+        print("Adding quotes entry to database")
+        database["quotes"] = []
 
 
 @app.on_event("shutdown")
@@ -32,12 +39,9 @@ def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
     You should not modify this function except for the return value.
     """
     now = datetime.now().replace(microsecond=0)
-    post = {
-        "name": name,
-        "message": message,
-        "time": now.isoformat(),
-    }
-    database["posts"].append(post)
+
+    quote = Quote(name=name, message=message, time=now.isoformat())
+    database["quotes"].append(quote)
 
     # You may modify the return value as needed to support other functionality
     return RedirectResponse("/", status.HTTP_303_SEE_OTHER)
