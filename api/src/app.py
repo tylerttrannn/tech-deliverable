44be51db2d1551fdd3237c3324978f14cd8eadf2
tylerttrannn <tylerttrann@gmail.com>
2024-11-08 22:11:36 -0800

    created a quotes route to retrieve quotes stored in the database

--- api/src/app.py
+++ api/src/app.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
-from datetime import datetime
-from typing import AsyncIterator
+from datetime import datetime, timedelta
+from typing import AsyncIterator, List
 
 from fastapi import FastAPI, Form, status
 from fastapi.responses import RedirectResponse
@@ -48,3 +48,37 @@ def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
 
 
 # TODO: add another API route with a query parameter to retrieve quotes based on max age
+
+@app.get("/quotes")
+async def retrieve_quotes(maxAge: str) -> List[Quote]:
+    """
+    Retrives a list of quotes given a maxAge paramater 
+    filtering via the week, month, year or all quotes
+    included 
+    """ 
+
+    if maxAge == "lastWeek":
+        cutoff = now - timedelta(weeks = 1 )
+
+    elif maxAge == "month":
+        cutoff = now - timedelta(days = 30)
+
+    elif maxAge == "year":
+        cutoff = now - timedelta(days = 365)
+    
+    else:
+        cutoff = None 
+    
+    filteredQuotes = []
+    now = datetime.now()
+
+    if "quotes" in database: 
+        for quote in database["quotes"]:
+            quote_time = datetime.strptime(quote["time"], "%Y-%m-%dT%H:%M:%S")
+
+            if quote_time >= cutoff or cutoff is None :
+                filteredQuotes.append(quote)
+
+    return filteredQuotes 
+
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 
 function App() {
+
+
+
 	return (
 		<div className="App">
 			{/* TODO: include an icon for the quote book */}
@@ -18,11 +21,14 @@ function App() {
 
 			<h2>Previous Quotes</h2>
 			{/* TODO: Display the actual quotes from the database */}
+
 			<div className="messages">
 				<p>Peter Anteater</p>
 				<p>Zot Zot Zot!</p>
 				<p>Every day</p>
 			</div>
+
+
 		</div>
 	);
 }
